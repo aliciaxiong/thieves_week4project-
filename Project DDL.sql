539946850485a4347f85c19b898bb844419b650c
Your Name aliciaxiong <aliciaxiong22@gmail.com>
2023-11-05 22:45:26 -0800

    first commit

---
+++ Project DDL.sql
@@ -0,0 +1,64 @@
+CREATE TABLE services list(
+    service_id SERIAL PRIMARY KEY,
+    service_name VARCHAR,
+    service_cost VARCHAR,
+); --finish DML
+
+CREATE TABLE services ticket(
+    ticket_id SERIAL PRIMARY KEY, 
+    mechanic_id INTEGER,
+    FOREIGN KEY mechanic_id REFERENCES services employee(employee_id)
+    vehicle_information INTEGER, 
+    FOREIGN KEY vehicle_information REFERENCES vehicle information(vehicle_id)
+    service_id INTEGER,
+    FOREIGN KEY service_id REFERENCES services list(service_id)
+    customer_id INTEGER, 
+    FOREIGN KEY customer_id REFERENCES customer(customer_id)
+    total_amount_charge VARCHAR
+);
+
+CREATE TABLE sales employees(
+    employee_id SERIAL PRIMARY KEY,
+    employee_firstname VARCHAR,
+    employee_lastname VARCHAR,
+    employee_dob VARCHAR,
+    employee_phonenumber VARCHAR
+); --finish DML
+
+CREATE TABLE services employees(
+    employee_id SERIAL PRIMARY KEY,
+    employee_firstname VARCHAR,
+    employee_lastname VARCHAR,
+    employee_dob VARCHAR,
+    employee_phonenumber VARCHAR
+); --finish DML
+
+CREATE TABLE vehicle information(
+    vehicle_id SERIAL PRIMARY KEY,
+    make VARCHAR,
+    model VARCHAR,
+    year INTEGER,
+    color VARCHAR,
+    style VARCHAR
+    VIN_DIGIT INTEGER
+);
+
+CREATE TABLE vehicle purchase agreement (
+    pruchase_agreement_id SERIAL PRIMARY KEY, 
+    customer_id INTEGER, 
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    employee_id INTEGER, 
+    FOREIGN KEY (employee_id) REFERENCES services employees(employee_id),
+    vehicle_information INTEGER,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle information(vehicle_id),
+    purchase_amount VARCHAR
+);
+
+CREATE TABLE customer profiles (
+    customer_id SERIAL PRIMARY KEY, 
+    customer_firstname VARCHAR,
+    customer_lastname VARCHAR,
+    customer_address VARCHAR,
+    customer_phonenumber VARCHAR,
+    profile_notes VARCHAR
+);
---
+++ Project DML.sql
@@ -0,0 +1,117 @@
+-- SERVICES LIST 
+INSERT INTO (services list
+    service_name,
+    service_cost,
+) VALUES (
+    'tire rotation',
+    '$0.00'
+) (
+    'oil change',
+    '$60-$100'
+) (
+    'a/c racharge services',
+    '$50-$250'
+) (
+    'battery replacement',
+    '$45-$300'
+) (
+    'alternator belt replacement',
+    '$70-$200'
+) (
+    'timing belt replacement',
+    '$300-$1,000'
+) (
+    'clutch replacement',
+    '$1,200-$1,400'
+) (
+    'starter motor replacement',
+    '$150-$1,100+'
+) (
+    'water pump replacement',
+    '$200-$700'
+) (
+    'brake pad replacement',
+    '$100-$300'
+) (
+    'diagnostic inspection',
+    '$20-$500'
+) (
+    'car routine maintenance'
+    '$50-$200'
+) 
+
+-- SERVICE EMPLOYEES 
+INSERT INTO (services employees
+    employee_firstname,
+    employee_lastname,
+    employee_dob,
+    employee_phonenumber
+) VALUES (
+    'Chris',
+    'Garfield',
+    '01/03/1978',
+    '345-233-1233'
+) (
+    'Naomi',
+    'Frite',
+    '04/23/1987',
+    '563-674-2352'
+)  (
+    'John',
+    'Etrimist',
+    '02/29/1990',
+    '313-567-9239'
+) (
+    'Patrick',
+    'Linder',
+    '06/06/1991',
+    '909-234-2323'
+) (
+    'Justin',
+    'Hong',
+    '09/19/1986',
+    '566-344-7646'
+) (
+    'Mino',
+    'Lee',
+    '10/15/1982',
+    '989-234-5442'
+) 
+
+--sales employees 
+INSERT INTO (sales employees
+    employee_firstname,
+    employee_lastname,
+    employee_dob,
+    employee_phonenumber
+) VALUES (
+    'Shannon'
+    'Mcneal'
+    '05/23/2000'
+    '673-563-5632'
+) (
+    'Leanne'
+    'Shantell'
+    '05/01/1988'
+    '353-676-2311'
+) (
+    'William'
+    'Nicks'
+    '07/23/1985'
+    '980-389-4321'
+) (
+    'Harold'
+    'Hepplemen'
+    '11/20/1980'
+    '653-453-2134'
+) (
+    'Michael',
+    'Showman',
+    '10/10/1980',
+    '877-564-3456'
+) (
+    'Hunter',
+    'Dale',
+    '04/13/2000',
+    '343-342-6566'
+)
